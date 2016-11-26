16d499e48addb2194317f95d0a95d94c75e6c835
Japaka90 <japaka@yandex.ru>
2016-11-27 00:35:49 +0300

    почти готова вторая задача(доделать год)

--- time.js
+++ time.js
@@ -1,2 +1,33 @@
-'use strict';
+//'use strict';
 
+function initializeClock(time, option) {
+    var clock = document.querySelector('.clock');
+    time = time || new Date();
+    option = option || ['hours', 'minutes', 'seconds'];
+
+    var format = '';
+     
+    if(option.includes('year')){
+      format += 'Year: ' + time.getFullYear() + ' ';
+    }
+    if(option.includes('month')){
+      format += 'Month: ' + Number(time.getMonth() + 1) + ' ';
+    }
+    if(option.includes('day')){
+      format += 'Day: ' + time.getDate() + ' ';
+    }
+    if(option.includes('hours')){
+      format += 'Hours: ' + time.getHours() + ' ';
+    }
+    if(option.includes('minutes')){
+      format += 'Minutes: ' + time.getMinutes() + ' ';
+    }
+    if(option.includes('seconds')){
+      format += 'Seconds: ' + time.getSeconds() + ' ';
+    }        
+    clock.innerHTML = format;
+    
+    setTimeout('initializeClock()', 1000)
+}
+
+ initializeClock(new Date(2014, 6, 14), ['year', 'month', 'day', 'seconds']);
